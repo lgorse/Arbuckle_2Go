ab119dee01e261db7bf8f13cfe1e6c54f0f5cc21
lgorse <lgorse@stanford.edu>
2012-12-30 11:47:41 -0800

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Arbuckle2Go::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arbuckle2Go::Application.config.secret_token = 'b74e4edf3e4f09a2864c7d4f59fa2a49c916aa9b6396f8e74ab4d147eb89a97fe70db8f1d62db581593a3ea2c1cbf631703d82a57bb1752e40df66acd2d95a96'
